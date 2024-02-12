b528961c10c42cd6913886655146a9aa0c6451a1
Syakigrodan <118246839+Syakigrodan@users.noreply.github.com>
2024-02-12 08:21:00 +0700

    morning

--- app/Http/Controllers/CashierController.php
+++ app/Http/Controllers/CashierController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Models\ActivityHistory;
 use App\Models\DetailTransaction;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class CashierController extends Controller
 {
